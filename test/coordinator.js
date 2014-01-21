6950f5d972a53b0211fb8f85d8c723cc7fa8dd37
Travis McHattie <travis.mchattie@thinksolid.com>
2014-01-20 16:24:45 -0800

    Updated documentation and tests.

---
+++ test/coordinator.js
@@ -0,0 +1,51 @@
+require('chai').should();
+
+describe('Grasshopper core - coordinator', function(){
+    'use strict';
+
+    var coordinator = require('../lib/runners/coordinator');
+
+    it('coordinator should see the changes made to kontx by it\' middleware.', function(done) {
+        coordinator.use('example.method', [
+            function(kontx, next){
+                kontx.payload.test = 0;
+                next();
+            },
+            function(kontx, next){
+                kontx.payload.a = 1;
+                next();
+            }
+        ]);
+
+        coordinator.handle('example.method', {payload:{}}).then(function(payload){
+            payload.test.should.equal(0);
+            payload.a.should.equal(1);
+        }).done(function(){
+            done();
+        });
+    });
+
+    it('coordinator should notify when there is an error in the chain.', function(done) {
+        coordinator.use('example.method', [
+            function(kontx, next){
+                kontx.payload.test = 0;
+                next(new Error('ERROR!!!!'));
+            },
+            function(kontx, next){
+                kontx.payload.ab = 1;
+                next();
+            }
+        ]);
+
+        coordinator.handle('example.method', {payload:{}})
+            .then(function(payload){
+                payload.ab.should.not.equal(1);
+            })
+            .fail(function(e){
+                e.should.not.be.undefined;
+            }).done(function(err){
+                done();
+            });
+    });
+
+});
