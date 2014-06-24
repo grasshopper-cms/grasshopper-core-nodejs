6e57113ce0393586e279d17e1cf3db2b39dcd755
Peter Ajtai <peter.ajtai@thinksolid.com>
2014-06-24 15:28:31 -0700

    Adding query builder - ++Version 0.13.0

---
+++ test/utilities/queryBuilder.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var queryBuilder    = require('../../lib/grasshopper').utilities.queryBuilder;
+
+require('chai').should();
+
+describe('Query builder', function() {
+
+    it('find by id', function() {
+        queryBuilder
+            .create()
+            .equals('_id', 0)
+            .build()
+            .should.deep.equal({
+                filters : [{ key : '_id', cmp : '=', value : 0 }]
+            });
+    });
+
+    it('distinct by type', function() {
+        queryBuilder
+            .create()
+            .inTypes(['ab', 'cd'])
+            .distinct('fields.firstname')
+            .build()
+            .should.deep.equal({
+                types: ['ab', 'cd'],
+                options : {
+                    distinct : 'fields.firstname'
+                }
+            });
+    });
+
+    it('should be able to find in field', function() {
+        queryBuilder
+            .create()
+            .inNodes(['abd'])
+            .inField('fields.authors.authorid', ['authorId'])
+            .build()
+            .should.deep.equal({
+                nodes : [
+                    'abd'
+                ],
+                filters : [
+                    {
+                        key: 'fields.authors.authorid',
+                        cmp: 'in',
+                        value: ['authorId']
+                    }
+                ]
+            });
+    });
+
+    it('should be able to find in field given they value as a plain old string', function() {
+        queryBuilder
+            .create()
+            .inNodes(['abd'])
+            .inField('fields.authors.authorid', 'authorId')
+            .build()
+            .should.deep.equal({
+                nodes : [
+                    'abd'
+                ],
+                filters : [
+                    {
+                        key: 'fields.authors.authorid',
+                        cmp: 'in',
+                        value: ['authorId']
+                    }
+                ]
+            });
+    });
+});
