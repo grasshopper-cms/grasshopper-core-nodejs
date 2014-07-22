131b9f66685eeb72aad70d8b1fb766f3895ddece
Travis McHattie <travis.mchattie@thinksolid.com>
2014-07-22 10:58:54 -0700

    Trying to get website build to work.

--- grunt/configs/buildGhPages.js
+++ grunt/configs/buildGhPages.js
@@ -2,11 +2,10 @@ module.exports = function (grunt) {
     'use strict';
 
     grunt.config('buildGhPages', {
-        ghPages : {},
         website : {
             options: {
                 build_branch: "gh-pages",
-                dist: "docs/_site",
+                dist: "./docs/_site",
                 pull: true
             }
         }
