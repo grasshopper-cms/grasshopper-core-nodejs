e03785458465729b1cb72eb9a2371c7b7cd3d44c
Travis McHattie <travis.mchattie@thinksolid.com>
2014-08-05 15:57:34 -0700

    Added ability to query off of an array of keys.

--- grunt/tasks/fixtures/mongodb/db/test/content.js
+++ grunt/tasks/fixtures/mongodb/db/test/content.js
@@ -33,7 +33,20 @@ module.exports = function(ObjectID) {
                 labelfield: 'testfield'
             },
             fields: {
-                testfield: 'testvalue',
+                testfield: 'customtestvalue',
+                testeroni: true
+            }
+        },
+        {
+            _id: ObjectID(ids[3]),
+            meta: {
+                type: ObjectID('524362aa56c02c0703000123'),
+                node: ObjectID('526d5179966a883540000006'),
+                labelfield: 'testfield'
+            },
+            fields: {
+                testfield: 'testvalue12345',
+                othertestfield: 'customtestvalue',
                 testeroni: true
             }
         }
