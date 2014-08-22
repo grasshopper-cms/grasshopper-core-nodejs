b514706017b9454a8c885b4c97a17708efaabf6b
Peter Ajtai <peter.ajtai@thinksolid.com>
2014-08-22 00:23:32 -0700

    Beginning of own dev server for core

---
+++ dev/server/index.js
@@ -0,0 +1,19 @@
+/* jshint node:true */
+'use strict';
+console.log('starting');
+var express = require('express'),
+    app = express(),
+    api = require('grasshopper-api'),
+    PORT = process.env.PORT || 3000;
+
+api = api(require('./ghapi.json'));
+app.use(api.router);
+app.use(express.static(__dirname + '/public/admin'));
+
+app.get('/admin*?', function(request, response) {
+    response.sendfile(__dirname + '/public/admin/index.html');
+});
+
+app.listen(PORT, function(){
+    console.log('Listening on: ' + PORT);
+});
